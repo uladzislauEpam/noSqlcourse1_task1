95455ec3270c419cbc195792f31554bdf11090af
CowOwner <maloi.promist@gmail.com>
2022-05-21 15:09:03 +0300

    - Added exception class for DB: DbException

---
+++ src/main/java/ua/epam/mishchenko/ticketbooking/exception/DbException.java
@@ -0,0 +1,12 @@
+package ua.epam.mishchenko.ticketbooking.exception;
+
+public class DbException extends RuntimeException {
+
+    public DbException(String message) {
+        super(message);
+    }
+
+    public DbException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
