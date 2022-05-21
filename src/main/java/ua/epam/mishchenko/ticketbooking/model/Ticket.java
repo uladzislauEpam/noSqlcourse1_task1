bb28fdc38865899f264f058b6f82d47e8e7d7aa9
CowOwner <maloi.promist@gmail.com>
2022-05-21 14:59:44 +0300

    - Init project

---
+++ src/main/java/ua/epam/mishchenko/ticketbooking/model/Ticket.java
@@ -0,0 +1,24 @@
+package ua.epam.mishchenko.ticketbooking.model;
+
+/**
+ * Created by maksym_govorischev.
+ */
+public interface Ticket {
+    public enum Category {STANDARD, PREMIUM, BAR}
+
+    /**
+     * Ticket Id. UNIQUE.
+     * @return Ticket Id.
+     */
+    long getId();
+    void setId(long id);
+    long getEventId();
+    void setEventId(long eventId);
+    long getUserId();
+    void setUserId(long userId);
+    Category getCategory();
+    void setCategory(Category category);
+    int getPlace();
+    void setPlace(int place);
+
+}
