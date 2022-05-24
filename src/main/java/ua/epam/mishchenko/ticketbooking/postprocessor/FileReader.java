f7566d9df4d5b754c8920c996ed84e6e77332cee
twistedmisted <maloi.promist@gmail.com>
2022-05-24 20:01:44 +0300

    - Updated logger to SLF4J for StorageBeanPostProcessor; - Added logger to FileReader

--- src/main/java/ua/epam/mishchenko/ticketbooking/postprocessor/FileReader.java
+++ src/main/java/ua/epam/mishchenko/ticketbooking/postprocessor/FileReader.java
@@ -1,5 +1,8 @@
 package ua.epam.mishchenko.ticketbooking.postprocessor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,18 +12,25 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
+    private static final Logger log = LoggerFactory.getLogger(FileReader.class);
+
     private File file;
 
     public Map<String, String> readPreparedDataFromFile() {
+        log.info("Trying to read prepared data from file: {}", file);
+
         String delimiter = "=";
         Map<String, String> map = new HashMap<>();
-        try(Stream<String> lines = Files.lines(file.toPath())){
+        try (Stream<String> lines = Files.lines(file.toPath())) {
             lines.filter(line -> line.contains(delimiter)).forEach(
                     line -> map.putIfAbsent(line.split(delimiter)[0], line.split(delimiter)[1])
             );
         } catch (IOException e) {
+            log.warn("Can not to retrieve prepared data from file: {}", file);
             throw new RuntimeException("Can not to retrieve prepared data from file", e);
         }
+
+        log.info("Data read successfully");
         return map;
     }
 
--- src/main/java/ua/epam/mishchenko/ticketbooking/postprocessor/StorageBeanPostProcessor.java
+++ src/main/java/ua/epam/mishchenko/ticketbooking/postprocessor/StorageBeanPostProcessor.java
@@ -1,22 +1,16 @@
 package ua.epam.mishchenko.ticketbooking.postprocessor;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import ua.epam.mishchenko.ticketbooking.db.Storage;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class StorageBeanPostProcessor implements BeanPostProcessor {
 
-    private static final Logger LOGGER = LogManager.getLogger(StorageBeanPostProcessor.class);
+    private static final Logger log = LoggerFactory.getLogger(StorageBeanPostProcessor.class);
 
     private FileReader fileReader;
 
@@ -31,22 +25,20 @@ public class StorageBeanPostProcessor implements BeanPostProcessor {
     }
 
     private Object process(Object bean) {
-        LOGGER.log(Level.INFO, "{}: processing bean of type {} during initialization",
+        log.info("{}: processing bean of type {} during initialization",
                 this.getClass().getSimpleName(), bean.getClass().getName());
         if (bean instanceof Storage) {
-            LOGGER.log(Level.INFO,
-                    "{}: inserting prepared data in bean of type {}",
+            log.info("{}: inserting prepared data in bean of type {}",
                     this.getClass().getSimpleName(), bean.getClass().getName());
             try {
                 Map<String, String> inMemoryStorage = fileReader.readPreparedDataFromFile();
                 Storage storage = (Storage) bean;
                 storage.setInMemoryStorage(inMemoryStorage);
-                LOGGER.log(Level.INFO,
-                        "{}: successfully inserting prepared data in bean of type {}",
+                log.info("{}: successfully inserting prepared data in bean of type {}",
                         this.getClass().getSimpleName(), bean.getClass().getName());
                 return storage;
             } catch (RuntimeException e) {
-                LOGGER.log(Level.ERROR, "{}: error while inserting prepared data in bean of type {}",
+                log.error("{}: error while inserting prepared data in bean of type {}",
                         this.getClass().getSimpleName(), bean.getClass().getName(), e);
             }
         }
