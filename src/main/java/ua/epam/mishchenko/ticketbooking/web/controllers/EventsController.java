5970a25c2eac877b8b886843b80a5ca3b4725716
twistedmisted <maloi.promist@gmail.com>
2022-05-25 20:40:31 +0300

    - Init controllers: EventsController, TicketsController, UsersController

---
+++ src/main/java/ua/epam/mishchenko/ticketbooking/web/controllers/EventsController.java
@@ -0,0 +1,10 @@
+package ua.epam.mishchenko.ticketbooking.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.epam.mishchenko.ticketbooking.facade.impl.BookingFacadeImpl;
+
+@Controller
+@RequestMapping("/events")
+public class EventsController {
+}
---
+++ src/main/java/ua/epam/mishchenko/ticketbooking/web/controllers/TicketsController.java
@@ -0,0 +1,9 @@
+package ua.epam.mishchenko.ticketbooking.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tickets")
+public class TicketsController {
+}
