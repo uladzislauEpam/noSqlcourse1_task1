210e7db5aa81b66cad1d6e7762d160feaa756b48
twistedmisted <maloi.promist@gmail.com>
2022-05-28 16:19:15 +0300

    - Added new dependencies to pom.xml; - Implemented custom HandlerExceptionResolver: CustomHandlerExceptionResolver; - Added a view for errors: error.html

---
+++ src/main/java/ua/epam/mishchenko/ticketbooking/web/handlers/CustomHandlerExceptionResolver.java
@@ -0,0 +1,22 @@
+package ua.epam.mishchenko.ticketbooking.web.handlers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CustomHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("message", ex.getMessage());
+        return new ModelAndView("error", model);
+    }
+}
